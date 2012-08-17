2f1aab3c2885750cb7f18ee0aa4e2a18bc42ac32
septor <patrickweaver@gmail.com>
2012-08-17 16:40:24 -0400

    Italian language file

---
+++ languages/Italian.php
@@ -0,0 +1,41 @@
+<?php
+
+// admin_config.php
+define("SOPLAN_CONFIGCAPTION_01", "Inserisci un nuovo Contenuto Codice");
+define("SOPLAN_CONFIGCAPTION_02", "Elenco Codici");
+
+define("SOPLAN_CONFIG_01", "Codice inserito con successo!");
+define("SOPLAN_CONFIG_02", "Impossibile inserire nuovo Codice!");
+define("SOPLAN_CONFIG_03", "Dati modificati con successo!");
+define("SOPLAN_CONFIG_04", "Errore nella modifica Dati.");
+define("SOPLAN_CONFIG_05", "Dati cancellati con successo!");
+define("SOPLAN_CONFIG_06", "Errore nella cancellazione Dati.");
+define("SOPLAN_CONFIG_07", "Contenuto del codice:");
+define("SOPLAN_CONFIG_08", "Per ottenere risultati ottimali cerca di non duplicare la visualizzazione del codice.");
+define("SOPLAN_CONFIG_09", "Ad esempio, non richiamare due volte uno stesso foglio di stile. Se da un lato infatti la presenza di un CSS  duplicato non comporta in s&egrave; nulla di grave, dall'altro potrebbe compromettere la funzionalit&agrave; di qualcos'altro.");
+define("SOPLAN_CONFIG_10", "Descrizione:");
+define("SOPLAN_CONFIG_11", "Usa qualcosa che ti ricordi come utilizzi il Codice (v. Elenco Codici sotto). Questo verr&agrave; mostrato in seguito nella pagina di configurazione e verr&agrave; visualizzato anche un commento HTML prima del codice.");
+define("SOPLAN_CONFIG_12", "Visualizza nelle seguenti Pagine:");
+define("SOPLAN_CONFIG_13", "Separa ogni pagina con una virgola. Non sono richiesti spazi. Non &egrave; richiesta nessuna struttura Directory.");
+define("SOPLAN_CONFIG_14", "Puoi ad esempio usare forum.php per impostare tutte le pagine del forum.");
+define("SOPLAN_CONFIG_15", "Puoi usare * per visualizzare in tutte le pagine.");
+define("SOPLAN_CONFIG_16", "<b>Attenzione:</b> Presta attenzione alle pagine che inserisci. Molti plugin utilizzano gli stessi nomi di file di paging, usa quindi con cautela in modo da non interrompere la loro funzionalit&agrave!");
+define("SOPLAN_CONFIG_17", "Chi pu&ograve; visualizzare questo codice?:");
+define("SOPLAN_CONFIG_18", "Seleziona il Gruppo Utenti a cui far visualizzare il codice.");
+define("SOPLAN_CONFIG_19", "Aggiungi ad Elenco Codici");
+define("SOPLAN_CONFIG_20", "ID");
+define("SOPLAN_CONFIG_21", "Contenuto Codice");
+define("SOPLAN_CONFIG_22", "Pagine");
+define("SOPLAN_CONFIG_23", "Gruppo Utenti");
+define("SOPLAN_CONFIG_24", "Salva");
+define("SOPLAN_CONFIG_25", "Sei sicuro di voler cancellare questi Dati?");
+define("SOPLAN_CONFIG_26", "Nessun dato di contenuto codici da visualizzare.");
+
+
+// plugin.php
+define("SOPLAN_PLUGIN_01", "Visualizza il codice su ogni pagina. Oh, puoi anche decidere di visualizzarlo solo su alcune pagine.");
+define("SOPLAN_PLUGIN_02", "Configura ShowOnPages");
+define("SOPLAN_PLUGIN_03", "&egrave; stato installato con successo.");
+define("SOPLAN_PLUGIN_04", "&egrave; stato aggiornato con successo.");
+
+?>
