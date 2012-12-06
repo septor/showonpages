b41fb325914caeeac06cb605a149465b7d8dd73b
septor <patrickweaver@gmail.com>
2012-12-05 20:49:04 -0500

    Spanish & Portuguese langauge files

---
+++ languages/Portuguese.php
@@ -0,0 +1,50 @@
+<?php
+/*
++---------------------------------------------------------------+
+|       Tradução Português(PT) -> Comunidade e107 Portugal
+|      	(http://www.e107pt.com), 2012
+|
+|       Released under the terms and conditions of the
+|       GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+
+// admin_config.php
+define("SOPLAN_CONFIGCAPTION_01", "Adicionar novo código");
+define("SOPLAN_CONFIGCAPTION_02", "Lista de códigos");
+
+define("SOPLAN_CONFIG_01", "O código foi adicionado com sucesso!");
+define("SOPLAN_CONFIG_02", "Falha ao adicionar o código!");
+define("SOPLAN_CONFIG_03", "Atualização bem-sucedida!");
+define("SOPLAN_CONFIG_04", "Erro na insersão do código!");
+define("SOPLAN_CONFIG_05", "Código excluído com sucesso!");
+define("SOPLAN_CONFIG_06", "Erro ao excluir o código!");
+define("SOPLAN_CONFIG_07", "<b>Conteúdo de código</b>");
+define("SOPLAN_CONFIG_08", "Para obter um bom resultado o código do aplicativo não pode ser duplicado.");
+define("SOPLAN_CONFIG_09", "<b>Exemplo:</b> chamar duas vezes uma folha de estilo não implica grandes riscos, porém outro tipo de código sim, afetando a funcionalidade da aplicação.");
+define("SOPLAN_CONFIG_10", "<b>Descrição</b>");
+define("SOPLAN_CONFIG_11", "Insira um detalhe para se lembrar do tipo de utilização do código em execusão (ver lista em abaixo).");
+define("SOPLAN_CONFIG_12", "<b>Apresentação em páginas</b>");
+define("SOPLAN_CONFIG_13", "# Separar cada página com uma vírgula e sem espaços.");
+define("SOPLAN_CONFIG_14", "# Definir todas as páginas do fórum usando forum.php.");
+define("SOPLAN_CONFIG_15", "# Pode usar (*) para exibir em todas as páginas.");
+define("SOPLAN_CONFIG_16", "<b>Atenção:</b> tenha muita atenção com as páginas que você vai usar. Alguns plugins podem usar os mesmos nomes de arquivo de página, tenha cuidado para não quebrar a funcionalidade do aplicativo.");
+define("SOPLAN_CONFIG_17", "<b>Quem pode ver</b>");
+define("SOPLAN_CONFIG_18", "Selecione a classe de utilizador que pode ver este aplicativo");
+define("SOPLAN_CONFIG_19", "Adicionar Código");
+define("SOPLAN_CONFIG_20", "ID");
+define("SOPLAN_CONFIG_21", "Código de aplicativos");
+define("SOPLAN_CONFIG_22", "Páginas");
+define("SOPLAN_CONFIG_23", "Classes");
+define("SOPLAN_CONFIG_24", "Salvar");
+define("SOPLAN_CONFIG_25", "Tem certeza de que deseja apagar esta entrada?");
+define("SOPLAN_CONFIG_26", "Sem conteúdo de código para mostrar.");
+
+
+// plugin.php
+define("SOPLAN_PLUGIN_01", "Este plugin lhe ajudará a inserir e mostra código de aplicativos em páginas.");
+define("SOPLAN_PLUGIN_02", "Configuração de ShowOnPages");
+define("SOPLAN_PLUGIN_03", "foi instalado com sucesso!");
+define("SOPLAN_PLUGIN_04", "foi atualizado com sucesso!");
+
+?>
---
+++ languages/Spanish.php
@@ -0,0 +1,50 @@
+<?php
+/*
++---------------------------------------------------------------+
+|       Traducción Spanish(ES) -> KANONimpresor
+|       (http://www.kanonimpresor.com), 2012
+|
+|       Released under the terms and conditions of the
+|       GNU General Public License (http://gnu.org).
++---------------------------------------------------------------+
+*/
+
+// admin_config.php
+define("SOPLAN_CONFIGCAPTION_01", "Añadir nuevo código");
+define("SOPLAN_CONFIGCAPTION_02", "Lista de códigos");
+
+define("SOPLAN_CONFIG_01", "El código fue añadido con éxito!");
+define("SOPLAN_CONFIG_02", "Fallo al añadir el código!");
+define("SOPLAN_CONFIG_03", "Actualización con éxito!");
+define("SOPLAN_CONFIG_04", "Error al añadir el código!");
+define("SOPLAN_CONFIG_05", "El código fue eliminado con éxito!");
+define("SOPLAN_CONFIG_06", "Error al eliminar el código!");
+define("SOPLAN_CONFIG_07", "<b>Contenido del código</b>");
+define("SOPLAN_CONFIG_08", "Para obtener buenos resultados no se debe duplicar el código.");
+define("SOPLAN_CONFIG_09", "<b>Ejemplo:</b> llamar dos veces a la misma hoja de estilo no tiene mucha importancia, pero otro tipo de código puede afectar el funcionamiento de la aplicación.");
+define("SOPLAN_CONFIG_10", "<b>Descripción</b>");
+define("SOPLAN_CONFIG_11", "Coloque un detalle para acordarse del tipo de aplicación añadido (ver lista abajo).");
+define("SOPLAN_CONFIG_12", "<b>Presentación en páginas</b>");
+define("SOPLAN_CONFIG_13", "# Separar cada página con una coma y sin espacios.");
+define("SOPLAN_CONFIG_14", "# Puede definir todas las páginas del foro usando forum.php.");
+define("SOPLAN_CONFIG_15", "# Puede utilizar (*) para mostrar el código en todas las páginas.");
+define("SOPLAN_CONFIG_16", "<b>Atención:</b> Preste mucha atención con las páginas que utiliza. Muchos plugins pueden utilizar los nombres de los archivos de las páginas.");
+define("SOPLAN_CONFIG_17", "<b>Quién puede ver</b>");
+define("SOPLAN_CONFIG_18", "Seleccionar la clase de usuario que puede ver esta aplicación.");
+define("SOPLAN_CONFIG_19", "Añadir Código");
+define("SOPLAN_CONFIG_20", "ID");
+define("SOPLAN_CONFIG_21", "Código de aplicaciones");
+define("SOPLAN_CONFIG_22", "Páginas");
+define("SOPLAN_CONFIG_23", "Clases");
+define("SOPLAN_CONFIG_24", "Salvar");
+define("SOPLAN_CONFIG_25", "¿Está seguro que desea eliminar esta entrada?");
+define("SOPLAN_CONFIG_26", "No hay ningún contenido de código para mostrar.");
+
+
+// plugin.php
+define("SOPLAN_PLUGIN_01", "Este plugin ayudará a añadir diversos tipos de código de aplicaciones en páginas");
+define("SOPLAN_PLUGIN_02", "Configurar ShowOnPages");
+define("SOPLAN_PLUGIN_03", "se ha instalado correctamente!");
+define("SOPLAN_PLUGIN_04", "se ha actualizado correctamente!");
+
+?>
