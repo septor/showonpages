bb02c80124a494358a3b5ec5cc361ff7bd7b8eb7
septor <patrickweaver@gmail.com>
2015-11-15 18:58:52 -0500

    quote swap, as per the LAN standards

--- languages/English_admin.php
+++ languages/English_admin.php
@@ -6,13 +6,13 @@
  * For additional information refer to the README.mkd file.
  *
  */
-define('LAN_SHOWONPAGES_MANAGE', 'Manage Content Codes');
-define('LAN_SHOWONPAGES_CREATE', 'Create New Content Code');
+define("LAN_SHOWONPAGES_MANAGE", "Manage Content Codes");
+define("LAN_SHOWONPAGES_CREATE", "Create New Content Code");
 
-define('LAN_SHOWONPAGES_CODE_01_A', 'Content Code');
-define('LAN_SHOWONPAGES_CODE_01_B', 'This is the content you want displayed in the head tags of the HTML output. Becareful with this, you could break things!');
-define('LAN_SHOWONPAGES_CODE_02', 'This is the description of your Content Code. It\'s displayed as an HTML comment directly above your Content Code.');
-define('LAN_SHOWONPAGES_CODE_03_A', 'Pages');
-define('LAN_SHOWONPAGES_CODE_03_B', 'The pages you want your content code displayed on. Enter * to display it on all pages.');
+define("LAN_SHOWONPAGES_CODE_01_A", "Content Code");
+define("LAN_SHOWONPAGES_CODE_01_B", "This is the content you want displayed in the head tags of the HTML output. Becareful with this, you could break things!");
+define("LAN_SHOWONPAGES_CODE_02", "This is the description of your Content Code. It's displayed as an HTML comment directly above your Content Code.");
+define("LAN_SHOWONPAGES_CODE_03_A", "Pages");
+define("LAN_SHOWONPAGES_CODE_03_B", "The pages you want your content code displayed on. Enter * to display it on all pages.");
 
 ?>
--- languages/English_global.php
+++ languages/English_global.php
@@ -6,8 +6,8 @@
  * For additional information refer to the README.mkd file.
  *
  */
-define('LAN_SHOWONPAGES_NAME', 'ShowOnPages');
-define('LAN_SHOWONPAGES_SUMM', 'Shows code on a page. Or many pages.');
-define('LAN_SHOWONPAGES_DISC', 'Display code on any page, every page, or onnly one page to a defined userclass.');
+define("LAN_SHOWONPAGES_NAME", "ShowOnPages");
+define("LAN_SHOWONPAGES_SUMM", "Shows code on a page. Or many pages.");
+define("LAN_SHOWONPAGES_DISC", "Display code on any page, every page, or only one page to a defined userclass.");
 
 ?>
